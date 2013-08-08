ee9ba479df37a6cfe3571bac894af59a79e73628
Peter Lyons <pete@peterlyons.com>
2013-08-07 23:37:31 -0600

    github deploy hook

---
+++ bin/github-hook.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var childProcess  = require("child_process")
+var http          = require("http")
+
+function deployDone (error, stdout, stderr) {
+  process.stdout.write(stdout)
+  process.stderr.write(stderr)
+  if (error) {
+    process.stderr.write("\ndeploy failed\n")
+  } else {
+    process.stdout.write("\ndeploy succeeded\n")
+  }
+}
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"})
+  res.end("Thank you, github!")
+  childProcess.exec(__dirname + "/go deploy", deployDone)
+}).listen(9125, "0.0.0.0")
