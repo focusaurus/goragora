9b53164a5d8a948ed3c2a434dc49041bbb41464e
Peter Lyons <pete@peterlyons.com>
2016-01-08 12:32:33 -0700

    tweak github hook

--- bin/github-hook.js
+++ bin/github-hook.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
-var childProcess  = require("child_process")
-var http          = require("http")
+var childProcess = require("child_process")
+var http = require("http")
 
-function deployDone (error, stdout, stderr) {
-  process.stdout.write(stdout)
-  process.stderr.write(stderr)
-  if (error) {
-    process.stderr.write("\ndeploy failed\n")
-  } else {
-    process.stdout.write("\ndeploy succeeded\n")
-  }
-}
+const PORT = process.env.PORT || 9125;
 
 http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"})
-  res.end("Thank you, github!\n")
+  function deployDone(error, stdout, stderr) {
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
+    if (error) {
+      res.writeHead(500, {
+        "Content-Type": "text/plain"
+      })
+      res.end("Oops\n" + error.message)
+      process.stderr.write("\ndeploy failed " + error.message)
+      return
+    }
+    res.writeHead(200, {
+      "Content-Type": "text/plain"
+    })
+    res.end("Thank you, github!\n")
+    process.stdout.write("\ndeploy succeeded\n")
+  }
   childProcess.exec(__dirname + "/deploy.sh", deployDone)
-}).listen(9125, "0.0.0.0")
+}).listen(PORT, "0.0.0.0")
+console.log("GGO github hook listening on", PORT)
