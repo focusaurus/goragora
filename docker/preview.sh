b2340f12f0f18e16b7f8452f4230c5553d11a22a
Peter Lyons <pete@peterlyons.com>
2018-05-16 10:28:50 -0600

    Put nginx config back in deploy directory

--- docker/preview.sh
+++ docker/preview.sh
@@ -4,7 +4,7 @@ source ./bin/lib/strict-mode.sh
 echo Load site at the following URL:
 echo http://stage.goragora.org:9121
 exec docker run --rm \
-  --volume "${PWD}/docker/goragora.org:/etc/nginx/conf.d/default.conf:ro" \
+  --volume "${PWD}/deploy/goragora.org:/etc/nginx/conf.d/default.conf:ro" \
   --volume "${PWD}/build:/home/goragora/goragora/build:ro" \
   --publish 127.0.0.1:9121:80 \
   nginx:1.14-alpine
