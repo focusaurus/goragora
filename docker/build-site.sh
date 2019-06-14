359b6aa2e0ef37e7e79fb5494fed15cdaae45765
Peter Lyons <pete@reactioncommerce.com>
2019-06-13 22:29:12 -0600

    feat: run docker as node user

--- docker/build-site.sh
+++ docker/build-site.sh
@@ -2,4 +2,6 @@
 cd "$(dirname "$0")/.."
 source ./bin/lib/strict-mode.sh
 docker run --rm --interactive --tty \
+  --user $(id -u) \
+  --env HOME=/home/node \
   --volume="${PWD}:/opt" goragora ./bin/build-site.sh
