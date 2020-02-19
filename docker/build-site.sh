f7159adf58066e2f59da7d7fb04908a576ccc3bc
Peter Lyons <pete@peterlyons.com>
2020-02-18 18:13:47 -0700

    Add bash strict mode boilerplate

--- docker/build-site.sh
+++ docker/build-site.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")/.."
+
+# Please Use Google Shell Style: https://google.github.io/styleguide/shell.xml
+
+# ---- Start unofficial bash strict mode boilerplate
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit  # always exit on error
+set -o errtrace # trap errors in functions as well
+set -o pipefail # don't ignore exit codes when piping output
+set -o posix    # more strict failures in subshells
+# set -x          # enable debugging
+
+IFS=$'\n\t'
+# ---- End unofficial bash strict mode boilerplate
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 source ./bin/lib/strict-mode.sh
 docker run --rm --interactive --tty \
-  --user $(id -u) \
+  --user "$(id -u)" \
   --env HOME=/home/node \
   --volume="${PWD}:/opt" goragora ./bin/build-site.sh
